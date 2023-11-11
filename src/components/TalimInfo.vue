f8f261a447bf80a767375fd1e449d06df2a9eac1
Mohir <bahtisat442@gmail.com>
2023-11-11 13:54:34 +0500

    animation duration fixed

--- src/components/TalimInfo.vue
+++ src/components/TalimInfo.vue
@@ -15,34 +15,55 @@
             />
             <p class="text-[#1B1B35] text-[18px]">Nazariy kurs</p>
           </div>
-          <div class="line1 text-[#E0E0E0] text-[30px] mt-[20px]">
+          <div
+            class="text-[#E0E0E0] text-[30px] mt-[20px]"
+            id="animated"
+            :class="{ line1: isVisible }"
+          >
             - - - - - -
           </div>
           <div class="lamp flex items-center justify-center flex-col gap-[5px]">
             <img src="/src/assets/icons/triogonometry.svg" alt="#" />
             <img
               src="/src/assets/icons/down-arrow.svg"
-              class="downArrow bg-[#E0E0E0] p-[8px] rounded-[50px]"
+              class="bg-[#E0E0E0] p-[8px] rounded-[50px]"
+              :class="{ downArrow: isVisible }"
               alt="#"
             />
             <p class="text-[#1B1B35] text-[18px]">Amaliy kurs</p>
           </div>
         </div>
         <div
-          class="line2 text-[#E0E0E0] text-[30px] flex items-end justify-end mt-[30px] ml-[230px] rotate-90"
+          class="text-[#E0E0E0] text-[30px] flex items-end justify-end mt-[30px] ml-[230px] rotate-90"
+          :class="{ line2: isVisible }"
         >
           - - - -
         </div>
         <div class="level1 flex items-center justify-center mt-[35px]">
           <div class="lamp flex items-center justify-center flex-col gap-[5px]">
-            <img src="/src/assets/icons/nurce.svg" alt="#">
-            <img src="/src/assets/icons/left-arrow.svg" class="leftArrow bg-[#E0E0E0] p-[8px] rounded-[50px]" alt="#">
+            <img src="/src/assets/icons/nurce.svg" alt="#" />
+            <img
+              src="/src/assets/icons/left-arrow.svg"
+              class="bg-[#E0E0E0] p-[8px] rounded-[50px]"
+              alt="#"
+              :class="{ leftArrow: isVisible }"
+            />
             <p class="text-[#1B1B35] text-[18px]">YHXX imtihoni</p>
           </div>
-          <div class="line3 text-[#E0E0E0] text-[30px] mt-[20px]">- - - - - -</div>
+          <div
+            class="text-[#E0E0E0] text-[30px] mt-[20px]"
+            :class="{ line3: isVisible }"
+          >
+            - - - - - -
+          </div>
           <div class="lamp flex items-center justify-center flex-col gap-[5px]">
-            <img src="/src/assets/icons/done_job.svg" alt="#">
-            <img src="/src/assets/icons/check-circle.svg" class="done bg-[#E0E0E0] p-[8px] rounded-[50px]" alt="#">
+            <img src="/src/assets/icons/done_job.svg" alt="#" />
+            <img
+              src="/src/assets/icons/check-circle.svg"
+              class="bg-[#E0E0E0] p-[8px] rounded-[50px]"
+              alt="#"
+              :class="{ done: isVisible }"
+            />
             <p class="text-[#1B1B35] text-[18px]">Ichki imtihon</p>
           </div>
         </div>
@@ -52,13 +73,44 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+  mounted() {
+    this.isVisible = this.isElementInViewport();
+    document.addEventListener("scroll", () => {
+      if (!this.isVisible) {
+        this.isVisible = this.isElementInViewport();
+      }
+    });
+  },
+  methods: {
+    isElementInViewport() {
+      const el = document.getElementById("animated");
+      if (!el) return;
+
+      var rect = el.getBoundingClientRect();
+
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <=
+          (window.innerWidth || document.documentElement.clientWidth)
+      );
+    },
+  },
+};
 </script>
 
 <style scoped>
 .animated-text {
   animation: colorChange 2s;
-  background: linear-gradient(to right, #E0E0E0, #1B1B35);
+  background: linear-gradient(to right, #e0e0e0, #1b1b35);
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
@@ -68,16 +120,16 @@ export default {};
 
 @keyframes colorChange {
   0% {
-    color: #E0E0E0;
+    color: #e0e0e0;
   }
   100% {
-    color: #1B1B35;
+    color: #1b1b35;
   }
 }
 
-.line1{
-  animation: colorChangeLine 2s ;
-  background: linear-gradient(to right, #E0E0E0, #407BFF);
+.line1 {
+  animation: colorChangeLine 2s;
+  background: linear-gradient(to right, #e0e0e0, #407bff);
   background-clip: text;
   background-size: 200% 100%;
   -webkit-background-clip: text;
@@ -86,86 +138,81 @@ export default {};
   animation-fill-mode: forwards;
 }
 
-
 @keyframes colorChangeLine {
   0% {
     background-position: 0% 0%;
-    color: #E0E0E0;
+    color: #e0e0e0;
   }
   100% {
     background-position: 100% 0%;
-    color: #407BFF;
+    color: #407bff;
   }
 }
 
-.downArrow{
-  animation: colorChangeDown 2s ;
+.downArrow {
+  animation: colorChangeDown 2s;
   animation-fill-mode: forwards;
   animation-delay: 1s;
 }
 
 @keyframes colorChangeDown {
   0% {
-    background: #E0E0E0;
+    background: #e0e0e0;
   }
   100% {
-    background: #407BFF;
+    background: #407bff;
   }
 }
 
-.line2{
-  animation: colorChangeLine2 2s ;
-  color: linear-gradient(to right, #E0E0E0, #407BFF);
+.line2 {
+  animation: colorChangeLine2 2s;
+  color: linear-gradient(to right, #e0e0e0, #407bff);
   animation-fill-mode: forwards;
   animation-delay: 2s;
 }
 
 @keyframes colorChangeLine2 {
   0% {
-
-    color: #E0E0E0;
+    color: #e0e0e0;
   }
   100% {
-
-    color: #407BFF;
+    color: #407bff;
   }
 }
 
-.done{
-  animation: colorChangeDown 2s ;
+.done {
+  animation: colorChangeDown 2s;
   animation-fill-mode: forwards;
   animation-delay: 3s;
 }
 
 @keyframes colorChangeDown {
   0% {
-    background: #E0E0E0;
+    background: #e0e0e0;
   }
   100% {
-    background: #407BFF;
+    background: #407bff;
   }
 }
 
-.line3{
-  animation: colorChangeLine3 2s ;
-  color: linear-gradient(to right, #E0E0E0, #407BFF);
+.line3 {
+  animation: colorChangeLine3 2s;
+  color: linear-gradient(to right, #e0e0e0, #407bff);
   animation-fill-mode: forwards;
   animation-delay: 4s;
 }
 
 @keyframes colorChangeLine3 {
   0% {
-
-    color: #E0E0E0;
+    color: #e0e0e0;
   }
   100% {
-
-    color: #407BFF;
+    color: #407bff;
   }
 }
 
-.leftArrow{
-  animation: colorChangeDown 2s ;
+.leftArrow {
+  animation: colorChangeDown 2s;
   animation-fill-mode: forwards;
   animation-delay: 5s;
 }
